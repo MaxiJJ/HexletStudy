017623201e7174cba3ee9519f3a9962e72a95bb4
maxij <maximgri93@gmail.com>
2019-12-24 03:18:58 +0300

    create naval battle. should output the number of total shots, but I suck.

--- src/MaxTask1.java
+++ src/MaxTask1.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 
 public class MaxTask1 {
     public static void main(String[] args) {
+        //Scanner
+        //read radius from console
         int radius = 5;
         double pi = 3.14;
-        double S = pi * Math.pow(radius,2);
+        double S = pi * Math.pow(radius, 2);
         double P = (2 * pi * radius);
-        System.out.print("Square = " + S + " Perimeter = " + Math.rint(100.0 * P)/100.0);
+//        System.out.printf("%.2f\n", P);
+        System.out.print("Square = " + S + " Perimeter = " + Math.rint(100.0 * P) / 100.0);
     }
 }
---
+++ src/SimpleDotComTestDrive.java
@@ -0,0 +1,64 @@
+import java.io.*;
+public class SimpleDotComTestDrive {
+    public static void main(String[] args) {
+        int numOfGuesses = 0;
+        GameHelper helper = new GameHelper();
+
+        SimpleDotCom theDotCom = new SimpleDotCom();
+        int randomNum = (int) (Math.random() * 5);
+
+        int[] locations = {randomNum, randomNum + 1, randomNum + 2};
+        theDotCom.setLocationCells(locations);
+        boolean isAlive = true;
+
+        while (isAlive == true) {
+            String guess = helper.getUserInput("Insert the number");
+            String result = theDotCom.checkYourself(guess);
+            numOfGuesses++;
+            if (result.equals("Sank")) {
+                isAlive = false;
+                System.out.println("You needed " + numOfGuesses + " attempts");
+            }
+        }
+    }
+}
+class GameHelper {
+    public String getUserInput(String prompt) {
+        String inputLine = null;
+        System.out.print(prompt + "");
+        try {
+            BufferedReader is = new BufferedReader (new InputStreamReader(System.in));
+            inputLine = is.readLine();
+            if (inputLine.length() == 0) return null;
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+        return inputLine;
+    }
+}
+
+class SimpleDotCom {
+
+    int[] locationCells;
+    int numOfHits = 0;
+
+    public void setLocationCells(int[] locs) {
+        locationCells = locs;
+    }
+    public String checkYourself(String stringGuess) {
+        int guess = Integer.parseInt(stringGuess);
+        String result = "Fault";
+        for (int cell : locationCells) {
+            if (guess == cell) {
+                result = "Hit";
+                numOfHits++;
+                break;
+            }
+        }
+        if (numOfHits == locationCells.length) {
+            result = "You win";
+        }
+        System.out.println(result);
+        return result;
+    }
+}
