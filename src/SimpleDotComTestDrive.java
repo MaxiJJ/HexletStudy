2343bed02bc6cccabb7152056ae09a3027b83ceb
maxij <maximgri93@gmail.com>
2020-01-12 20:08:23 +0300

    ...

--- src/SimpleDotComTestDrive.java
+++ src/SimpleDotComTestDrive.java
@@ -1,10 +1,12 @@
 import java.io.*;
-public class SimpleDotComTestDrive {
+import java.util.*;
+class SimpleDotComGame {
     public static void main(String[] args) {
         int numOfGuesses = 0;
         GameHelper helper = new GameHelper();
 
         SimpleDotCom theDotCom = new SimpleDotCom();
+        
         int randomNum = (int) (Math.random() * 5);
 
         int[] locations = {randomNum, randomNum + 1, randomNum + 2};
@@ -62,3 +64,8 @@ class SimpleDotCom {
         return result;
     }
 }
+
+
+
+
+
